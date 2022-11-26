502d03f9dc5fc6bd823d940007be7d49b2fa2909
Matteodeleo03 <117951922+Matteodeleo03@users.noreply.github.com>
2022-11-26 21:43:44 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,20 +4,18 @@ using namespace std;
 int main() {
       int n, i, primo = 1, secondo = 1, terzo;
     cin >> n;
-if (n==1)
-    cout << primo;
-    else if (n>1){
-        cout << primo << endl;
-        cout << secondo << endl;
-        for (i = 3; i <= n; i++) {
+if (n>0){
+       cout << primo;
+   cout << secondo;
+   while (true){
+       if((primo+secondo)<=n) {
+           cout << primo + secondo;
+           if (primo < secondo) { primo += secondo; }
+           else { secondo += primo; }
 
-            terzo = primo + secondo;
-            primo = secondo;
-            secondo = terzo;
-            cout << terzo << endl;
-        } }
+       }else{break;}
+       }
+   }
 
-
-}
 
    
